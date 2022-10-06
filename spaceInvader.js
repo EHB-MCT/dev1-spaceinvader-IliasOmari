c134a2234da0f857ad02bb68ac775482e3ad4944
IliasOmari <ilias.omari@student.ehb.be>
2022-10-06 19:17:35 +0200

    functie met achtergrond en eerste stuk van avatar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,30 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawAvatar();
+
+function drawAvatar() {
+    let canvas = document.querySelector('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext('2d');
+
+    // Achtergrond:
+    context.fillStyle = "black";
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(0, 300);
+    context.lineTo(300, 300);
+    context.lineTo(300, 0);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+    //Algemene kleur voor avatar:
+    context.fillStyle = "#43f60e";
+
+    // Boven Links:
+
+    context.fillRect(10, 25, 50, 100);
+    context.fillRect(25, 75, 80, 50);
+
+}
